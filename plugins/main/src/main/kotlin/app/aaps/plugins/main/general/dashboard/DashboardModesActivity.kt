e411726afe20987d4c5690eefc146afbdc5e9c4b
R. Barth <email@reiner-barth.de>
2026-01-30 09:13:43 +0100

    Fix rebase merge mismatch

--- plugins/main/src/main/kotlin/app/aaps/plugins/main/general/dashboard/DashboardModesActivity.kt
+++ plugins/main/src/main/kotlin/app/aaps/plugins/main/general/dashboard/DashboardModesActivity.kt
@@ -1,6 +1,7 @@
 package app.aaps.plugins.main.general.dashboard
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.view.isVisible
 import app.aaps.core.interfaces.automation.Automation
 import app.aaps.core.interfaces.resources.ResourceHelper
@@ -11,9 +12,7 @@ import app.aaps.plugins.main.databinding.ActivityDashboardModesBinding
 import app.aaps.plugins.main.general.dashboard.modes.DashboardModesController
 import com.google.android.material.button.MaterialButton
 import javax.inject.Inject
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.Dispatchers
+
 
 class DashboardModesActivity : TranslatedDaggerAppCompatActivity() {
 
@@ -28,8 +27,12 @@ class DashboardModesActivity : TranslatedDaggerAppCompatActivity() {
         binding = ActivityDashboardModesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        modeController = DashboardModesController( this, automation, resourceHelper)
+        binding.toolbar.title = resourceHelper.gs(R.string.dashboard_nav_modes)
+        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
 
+        val settingsItem = binding.toolbar.menu.add(0, 1, 0, "Settings")
+        settingsItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        settingsItem.setIcon(app.aaps.core.ui.R.drawable.ic_settings)
     }
 
     override fun onResume() {
@@ -49,15 +52,10 @@ class DashboardModesActivity : TranslatedDaggerAppCompatActivity() {
             ).apply {
                 text = event.title
                 setOnClickListener {
-                    modeController.runEventWithConfirmation(event) {
-                        lifecycleScope.launch(Dispatchers.IO) {
-                            automation.processEvent(event)
-                        }
-                        finish()
-                    }
+                    modeController.runEventWithConfirmation(event)
+                    finish()
                 }
             }
-
             binding.actionsContainer.addView(button)
         }
     }
