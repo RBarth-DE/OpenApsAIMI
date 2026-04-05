8dd201c0ba0508b973ab828d376f8e80b554a5ff
R. Barth <email@reiner-barth.de>
2026-04-05 09:27:29 +0200

    fix: RandomBgPlugin available with virtual pump without engineering mode

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/RandomBgPlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/RandomBgPlugin.kt
@@ -127,7 +127,7 @@ class RandomBgPlugin @Inject constructor(
     }
 
     override fun specialEnableCondition(): Boolean {
-        return isRunningTest() || virtualPump.isEnabled() && config.isEngineeringMode() || config.isEnabled(ExternalOptions.UNFINISHED_MODE)
+        return isRunningTest() || virtualPump.isEnabled() || config.isEngineeringMode() || config.isEnabled(ExternalOptions.UNFINISHED_MODE)
     }
 
     @SuppressLint("CheckResult")
